b66443e103d0f208f23abaf3e906342bbf0c2796
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-11-26 08:42:52 +0000

    ep 6. completed

--- tests/Factory/DinosaurFactoryTest.php
+++ tests/Factory/DinosaurFactoryTest.php
@@ -9,14 +9,39 @@ use PHPUnit\Framework\TestCase;
 
 class DinosaurFactoryTest extends TestCase
 {
+    /**
+     * @var DinosaurFactory
+     */
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new DinosaurFactory();
+    }
+
     public function testItGrowsAVelociraptor()
     {
-        $factory = new DinosaurFactory();
-        $dinosaur = $factory->growVelociraptor(5);
+        $dinosaur = $this->factory->growVelociraptor(5);
 
         $this->assertInstanceOf(Dinosaur::class, $dinosaur);
         $this->assertInternalType('string', $dinosaur->getGenus());
         $this->assertSame('Velociraptor', $dinosaur->getGenus());
         $this->assertSame(5, $dinosaur->getLength());
+    }
+
+    public function testItGrowsATriceratops()
+    {
+        $this->markTestIncomplete('waiting for from GenLab');
+    }
+
+    public function testItGrowsABabyVelociraptor()
+    {
+        if (!class_exists('Nanny')) {
+            $this->markTestSkipped('no one to take care the raptor');
+        }
+
+        $dinosaur = $this->factory->growVelociraptor(1);
+        $this->assertSame(1, $dinosaur->getLength());
+
     }
 }
