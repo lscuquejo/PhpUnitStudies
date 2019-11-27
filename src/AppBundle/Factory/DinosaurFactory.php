6886d94aaa41e2da869498413b828bea476a3410
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-11-27 09:01:06 +0000

    ep 12. completed

--- src/AppBundle/Factory/DinosaurFactory.php
+++ src/AppBundle/Factory/DinosaurFactory.php
@@ -3,9 +3,17 @@
 namespace AppBundle\Factory;
 
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Service\DinosaurLengthDeterminator;
 
 class DinosaurFactory
 {
+    private $dinosaurLengthDeterminator;
+
+    public function __construct(DinosaurLengthDeterminator $dinosaurLengthDeterminator)
+    {
+        $this->dinosaurLengthDeterminator = $dinosaurLengthDeterminator;
+    }
+
     public function growVelociraptor(int $length): Dinosaur
     {
         return $this->createDinosaur('Velociraptor', true, $length);
@@ -20,7 +28,7 @@ class DinosaurFactory
     {
         // defaults
         $codeName = 'InG-' . random_int(1, 99999);
-        $length = $this->getLengthFromSpecification($specification);
+        $length = $this->dinosaurLengthDeterminator->getLengthFromSpecification($specification);
         $isCarnivorous = false;
 
 
@@ -41,30 +49,5 @@ class DinosaurFactory
         $dinosaur->setLength($length);
 
         return $dinosaur;
-    }
-
-    private function getLengthFromSpecification(string $specification): int
-    {
-        $availableLengths = [
-            'huge' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            'omg' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            '😱' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            'large' => ['min' => Dinosaur::LARGE, 'max' => Dinosaur::HUGE - 1],
-        ];
-        $minLength = 1;
-        $maxLength = Dinosaur::LARGE - 1;
-
-        foreach (explode(' ', $specification) as $keyword) {
-            $keyword = strtolower($keyword);
-
-            if (array_key_exists($keyword, $availableLengths)) {
-                $minLength = $availableLengths[$keyword]['min'];
-                $maxLength = $availableLengths[$keyword]['max'];
-
-                break;
-            }
-        }
-
-        return random_int($minLength, $maxLength);
     }
 }
